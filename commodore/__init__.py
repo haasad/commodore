3929c953561207935489534727ec8a04f30cf007
haasad <haasad@users.noreply.github.com>
2021-06-24 13:33:36 +0200

    Bump version to v0.0.1 to test pypi upload

--- commodore/__init__.py
+++ commodore/__init__.py
@@ -4,7 +4,7 @@ Commodore. Build dynamic inventories and compile catalogs with Kapitan
 
 __url__ = "https://github.com/projectsyn/commodore/"
 __git_version__ = "0"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 # provide Commodore installation dir as variable that can be imported
 from pathlib import Path as P
