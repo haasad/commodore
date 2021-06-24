ad82498defd6def23dceb61b2b200ab90f6b6c2e
haasad <haasad@users.noreply.github.com>
2021-06-24 14:01:20 +0200

    Use "syn-commodore" to detect package version

--- commodore/__init__.py
+++ commodore/__init__.py
@@ -5,7 +5,7 @@ from importlib_metadata import version
 
 __url__ = "https://github.com/projectsyn/commodore/"
 __git_version__ = "0"
-__version__ = version(__package__)
+__version__ = version("syn-commodore")
 
 # provide Commodore installation dir as variable that can be imported
 from pathlib import Path as P
