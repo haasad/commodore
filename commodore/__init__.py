7e3fac6a1d7d4ad5b92543e42c37093fa225338d
haasad <haasad@users.noreply.github.com>
2021-06-24 13:43:38 +0200

    Try potential fix for missing metadata
    see https://github.com/python-poetry/poetry/issues/1036

--- commodore/__init__.py
+++ commodore/__init__.py
@@ -1,10 +1,11 @@
 """
 Commodore. Build dynamic inventories and compile catalogs with Kapitan
 """
+from importlib_metadata import version
 
 __url__ = "https://github.com/projectsyn/commodore/"
 __git_version__ = "0"
-__version__ = "0.0.1"
+__version__ = version(__package__)
 
 # provide Commodore installation dir as variable that can be imported
 from pathlib import Path as P
